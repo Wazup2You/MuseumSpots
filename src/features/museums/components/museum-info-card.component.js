fbbf02e0ed8b2f7782b7593bc5f5e439e6a4fff3
Weifang <wazup2you@gmail.com>
2022-06-09 14:00:42 +0200

    Added rating and opening

--- src/features/museums/components/museum-info-card.component.js
+++ src/features/museums/components/museum-info-card.component.js
@@ -1,7 +1,11 @@
 import React from "react";
-import styled from "styled-components";
-import { Text, StyleSheet } from "react-native";
+import styled from "styled-components/native";
+import { Text, Image, View } from "react-native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
 
 const MuseumCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -12,9 +16,34 @@ const MuseumCardCover = styled(Card.Cover)`
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
 const Title = styled(Text)`
-  padding: ${(props) => props.theme.space[3]};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
   color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
 `;
 
 export const MuseumInfoCard = ({ museum = {} }) => {
@@ -27,13 +56,36 @@ export const MuseumInfoCard = ({ museum = {} }) => {
     address = "Eenadresstraat 12",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = museum;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
 
   return (
     <MuseumCard elevation={5}>
       <MuseumCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            <View />
+            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            <View />
+            <Image source={{ uri: icon }} />
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
     </MuseumCard>
   );
 };
