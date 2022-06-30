61e764035b8a033c45c97a3855660260407a760c
Weifang <wazup2you@gmail.com>
2022-06-30 10:22:28 +0200

    Storing favorites

--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -3,5 +3,5 @@ import React from "react";
 import { CompactMuseumInfo } from "../../../components/museum/compact-museum-info.component";
 
 export const MapCallout = ({ museum }) => (
-  <CompactMuseumInfo museum={museum} />
+  <CompactMuseumInfo isMap museum={museum} />
 );
--- src/services/favorites/favorites.context.js
+++ src/services/favorites/favorites.context.js
@@ -1,9 +1,35 @@
-import React, { createContext, useState } from "react"
+import React, { createContext, useState, useEffect } from "react"
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const FavoritesContext = createContext();
 
 export const FavoritesContextProvider = ({ children }) => {
     const [favorites, setFavorites] = useState([]);
+
+    const saveFavorites = async (value) => {
+        try {
+          const jsonValue = JSON.stringify(value)
+          await AsyncStorage.setItem('@favorites', jsonValue)
+        } catch (e) {
+          // saving error
+          console.log("error storing", e);
+        }
+      }
+
+      
+const loadFavorites = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@favorites')
+      if(value !== null) {
+        // value previously stored
+        setFavorites(JSON.parse(value));
+      }
+    } catch(e) {
+      // error reading value
+      console.log("error loading", e)
+    }
+  }
+  
 
     const add = (museum) => {
         setFavorites([...favorites, museum]);
@@ -16,6 +42,15 @@ export const FavoritesContextProvider = ({ children }) => {
 
         setFavorites(newFavorites);
     }
+
+    useEffect(() => {
+        loadFavorites(favorites);
+    }, []);
+
+    useEffect(() => {
+        saveFavorites(favorites);
+    }, [favorites]);
+
     return (
         <FavoritesContext.Provider value={{
             favorites,
